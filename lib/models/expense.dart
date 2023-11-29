db09c0a0f7f906860e249c14a9f2ce776cf31e27
salvo <salvoborda@gmail.com>
2023-11-29 23:42:38 +0100

    first commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+const uuid = Uuid();
+
+final formatter = DateFormat('dd/MM/yyyy');
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4(); //con questo 'costrutto' genereiamo un id e lo assegniamo
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
